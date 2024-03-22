dbba76abdc73d2626c91964d929f087773072ac3
CONTEMPL8git <christopherloch@hotmail.com>
2024-03-21 22:35:47 -0500

    I had not saved in PgAdmin after the last select, so did and now re-pushing

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -60,4 +60,10 @@ REFERENCES "category" ("category_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
-SELECT category
+SELECT * FROM category
+
+SELECT * FROM subcategory
+
+SELECT * FROM campaign
+
+SELECT * FROM contacts
